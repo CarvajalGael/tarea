993f51e823146ca604382d3fa89d2e5750f04035
CarvajalGael <23308060610312@cetis61.edu.mx>
2026-04-13 09:00:35 -0600

    act

--- src/views/LoginView.py
+++ src/views/LoginView.py
@@ -1,8 +1,18 @@
 import flet as ft
 
 def LoginView(page, auth:contrpoller):
-    email_inputt = ft.TextField(label="Correo electronico", width=350, border_radius=10)
+    email_input = ft.TextField(label="Correo electronico", width=350, border_radius=10)
     pass_input = ft.TextField (label="Contraseña", password=True can_reveal_password=True, width=350, border_radius=10)
     
     def login_click(e):
-        user, msg = au
+        user, msg = auth_controller,login(email_input.value, pass_input.value)
+        if user:
+            page.session.set("user", user) #Guardamos sesion
+        else:
+            page.snack_bar = ft.SnackBar(ft.Text(msg))
+            page.snack_bar.open = True
+            page.update()
+            
+    return ft.view("/", [
+        ft.AppBar(title=ft.Text("SIGE - Login"), )
+    ])
