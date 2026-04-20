35bc81df3f6497c9a4fcbc22f8da9c385a1eb886
CarvajalGael <23308060610312@cetis61.edu-mx>
2026-04-20 08:37:49 -0600

    act

--- src/controllers/UserController.py
+++ src/controllers/UserController.py
@@ -8,7 +8,7 @@ class AuthController:
         
     def registrar_usuario(self, nombre, email, password):
         try:
-            nuevo_usuario = UsuarioSchema(nombre=nombre, email=email, password=password)
+            nuevo_usuario = UsuariosSchema(nombre=nombre, email=email, password=password)
             success = self.model.registrar(nuevo_usuario)
             return success, "Usuario creado correctamente"
         except  ValidationError as e:
