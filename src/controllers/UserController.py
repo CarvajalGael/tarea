ff0a75622c642037a40cddf68b1e311cf9c3ea40
CarvajalGael <23308060610312@cetis61.edu.mx>
2026-05-05 19:34:08 -0600

    Refactor registrar_usuario to include additional fields

--- src/controllers/UserController.py
+++ src/controllers/UserController.py
@@ -2,14 +2,36 @@ from models.UserModel import UsuarioModel
 from models.schemasModel import UsuariosSchema
 from pydantic import ValidationError
 
+
 class AuthController:
     def __init__(self):
         self.model = UsuarioModel()
-        
-    def registrar_usuario(self, nombre, email, password):
+
+    def registrar_usuario(
+        self,
+        nombre,
+        apellido,
+        email,
+        password,
+        telefono=None,
+        foto_perfil=None
+    ):
         try:
-            nuevo_usuario = UsuariosSchema(nombre=nombre, email=email, password=password)
+            nuevo_usuario = UsuariosSchema(
+                nombre=nombre,
+                apellido=apellido,
+                email=email,
+                password=password,
+                telefono=telefono,
+                foto=foto_perfil
+            )
+
             success = self.model.registrar(nuevo_usuario)
-            return success, "Usuario creado correctamente"
-        except  ValidationError as e:
-            return False, e.errors()[0]['msg']
+
+            if success:
+                return True, "Usuario creado correctamente"
+
+            return False, "No se pudo registrar el usuario"
+
+        except ValidationError as e:
+            return False, e.errors()[0]["msg"]
