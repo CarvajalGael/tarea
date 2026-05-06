23f99de5d6b73480d7718abc1c921c37d6fa7948
CarvajalGael <23308060610312@cetis61.edu.mx>
2026-05-05 19:10:41 -0600

    Add UsuarioFormSchema with additional user fields

--- src/models/schemasModel.py
+++ src/models/schemasModel.py
@@ -16,7 +16,22 @@ class TareaSchems(BaseModel):
     descripcion: Optional[str] = None
     prioridad: str = "media"
     clasificacion: str = "personal"   
-    
+
+
 class UsuarioAlta(BaseModel):
     email: EmailStr
     password: str = Field(min_length=8)
+
+class UsuarioFormSchema(BaseModel):
+    # Campos que se piden en el formulario, con validaciones específicas
+    nombre: str = Field(min_length=2, max_length=30)
+    apellido: str = Field(min_length=8, max_length=30)
+    email: EmailStr
+    password: str = Field(min_length=8)
+    telefono: Optional[str] = Field(None, min_length=10, max_length=15)
+    foto: Optional[str] = None  # URL o ruta de la foto del usuario
+
+    # Campos que no se piden en el formulario, pero son necesarios a la hora de agregar al usuario en base de datos
+    activo: bool = True
+    fecha_registro: datetime = Field(default_factory=datetime.now)
+    ultimo_ingreso: datetime = Field(default_factory=datetime.now)
